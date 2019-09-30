e01dda7218b4390aa5b9e8280be109761c8fc576
q191201771 <191201771@qq.com>
2019-09-30 09:59:35 +0800

    new package: nazajson

---
+++ pkg/nazajson/nazajson.go
@@ -0,0 +1,44 @@
+package nzjson
+
+import (
+	"encoding/json"
+	"strings"
+)
+
+type JSON struct {
+	//raw []byte
+	m map[string]interface{}
+}
+
+func New(raw []byte) (JSON, error) {
+	var j JSON
+	err := j.Init(raw)
+	return j, err
+}
+
+func (j *JSON) Init(raw []byte) error {
+	return json.Unmarshal(raw, &j.m)
+}
+
+func (j *JSON) Exist(path string) bool {
+	return exist(j.m, path)
+}
+
+func exist(m map[string]interface{}, path string) bool {
+	ps := strings.Split(path, ".")
+
+	if len(ps) > 1 {
+		v, ok := m[ps[0]]
+		if !ok {
+			return false
+		}
+		mm, ok := v.(map[string]interface{})
+		if !ok {
+			return false
+		}
+		return exist(mm, ps[1])
+	}
+
+	_, ok := m[ps[0]]
+	return ok
+}

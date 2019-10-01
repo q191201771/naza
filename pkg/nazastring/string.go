444e450e5802d4941745126be213562b4a035496
q191201771 <191201771@qq.com>
2019-10-01 09:44:55 +0800

    new package nazastring

---
+++ pkg/nazastring/string.go
@@ -0,0 +1,24 @@
+package nazastring
+
+import "unsafe"
+
+type sliceT struct {
+	array unsafe.Pointer
+	len int
+	cap int
+}
+
+type stringStruct struct {
+	str unsafe.Pointer
+	len int
+}
+
+func SliceByteToStringTmp(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+func StringToSliceByteTmp(s string) []byte {
+	str := (*stringStruct)(unsafe.Pointer(&s))
+	ret := sliceT{array: unsafe.Pointer(str.str), len: str.len, cap: str.len}
+	return *(*[]byte)(unsafe.Pointer(&ret))
+}

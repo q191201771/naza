5d8805c735f7399e17fd02bbbc032fead803c2fb
q191201771 <191201771@qq.com>
2019-10-07 10:43:26 +0800

    1. new package nazastring 2. badge line size

---
+++ pkg/nazastring/string_test.go
@@ -0,0 +1,56 @@
+package nazastring
+
+import (
+	"bytes"
+	"github.com/q191201771/naza/pkg/assert"
+	"testing"
+)
+
+var inbuf = bytes.Repeat([]byte{'1', '2', '3', '4'}, 5678)
+var instr = string(inbuf)
+
+func TestSliceByteToStringTmp(t *testing.T) {
+	str := SliceByteToStringTmp(inbuf)
+	assert.Equal(t, instr, str)
+}
+
+func TestStringToSliceByteTmp(t *testing.T) {
+	buf := StringToSliceByteTmp(instr)
+	assert.Equal(t, inbuf, buf)
+}
+
+func BenchmarkSliceByteToStringOrigin(b *testing.B) {
+	var str string
+
+	for i := 0; i < b.N; i++ {
+		str = string(inbuf)
+	}
+	assert.Equal(b, instr, str)
+}
+
+func BenchmarkSliceByteToStringTmp(b *testing.B) {
+	var str string
+
+	for i := 0; i < b.N; i++ {
+		str = SliceByteToStringTmp(inbuf)
+	}
+	assert.Equal(b, instr, str)
+}
+
+func BenchmarkStringToSliceByteOrigin(b *testing.B) {
+	var buf []byte
+
+	for i := 0; i < b.N; i++ {
+		buf = []byte(instr)
+	}
+	assert.Equal(b, buf, inbuf)
+}
+
+func BenchmarkStringToSliceByteTmp(b *testing.B) {
+	var buf []byte
+
+	for i := 0; i < b.N; i++ {
+		buf = StringToSliceByteTmp(instr)
+	}
+	assert.Equal(b, buf, inbuf)
+}
